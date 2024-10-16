1d0830b240c06533d22ee2520a0bc98217955f5d
cambrenmwilliams <157054960+cambrenmwilliams@users.noreply.github.com>
2024-10-16 00:06:06 -0500

    Update project3.cpp

--- project3.cpp
+++ project3.cpp
@@ -2,70 +2,33 @@
 using namespace std;
 
 class CPUJob {
-    public:
-        int job_id; // Unique identifier for the job
-        int priority; // Priority level of the job (1-10)
-        int job_type; // Job type (1-10)
-        int cpu_time_consumed; // Total CPU time consumed by the job
-        int memory_consumed; // Total memory consumed thus far
-        CPUJob(int job_id, int priority, int job_type, int cpu_time_consumed,
-                int memory_consumed); // Constructor
-        void display(); // Display the job details
+public:
+    int job_id; // Unique identifier for the job
+    int priority; // Priority level of the job (1-10)
+    int job_type; // Job type (1-10)
+    int cpu_time_consumed; // Total CPU time consumed by the job
+    int memory_consumed; // Total memory consumed thus far
+    CPUJob(int job_id, int priority, int job_type, int cpu_time_consumed, int memory_consumed); // Constructor
+    void display(); // Display the job details
+};
 
-        int getJobId() const;
-        void setJobId(int job_id);
-
-        int getPriority() const;
-        void setPriority(int priority);
-
-        int getJobType() const;
-        void setJobType(int job_type);
-
-        int getCpuTimeConsumed() const;
-        void setCpuTimeConsumed(int cpu_time_consumed);
-
-        int getMemoryConsumed() const;
-        void setMemoryConsumed(int memory_consumed);
-    };
-
-CPUJob::CPUJob(int job_id, int priority, int job_type, int cpu_time_consumed,
-        int memory_consumed) {
-    this->job_id = job_id;
-    this->priority = priority;
-    this->job_type = job_type;
-    this->cpu_time_consumed = cpu_time_consumed;
-    this->memory_consumed = memory_consumed;
-}
+CPUJob::CPUJob(int job_id, int priority, int job_type, int cpu_time_consumed, int memory_consumed)
+    : job_id(job_id), priority(priority), job_type(job_type), cpu_time_consumed(cpu_time_consumed), memory_consumed(memory_consumed) {}
 
 void CPUJob::display() {
     cout << "Job ID: " << job_id << ", Priority: " << priority << ", Job Type: "
-    << job_type << ", CPU Time Consumed: " << cpu_time_consumed << ", Memory Consumed: "
-    << memory_consumed << endl;
+         << job_type << ", CPU Time Consumed: " << cpu_time_consumed << ", Memory Consumed: "
+         << memory_consumed << endl;
 }
-
-int CPUJob::getJobId() const { return job_id; }
-void CPUJob::setJobId(int job_id) { CPUJob::job_id = job_id; }
-
-int CPUJob::getPriority() const { return priority; }
-void CPUJob::setPriority(int priority) { CPUJob::priority = priority; }
-
-int CPUJob::getJobType() const { return job_type; }
-void CPUJob::setJobType(int job_type) { CPUJob::job_type = job_type; }
-
-int CPUJob::getCpuTimeConsumed() const { return cpu_time_consumed; }
-void CPUJob::setCpuTimeConsumed(int cpu_time_consumed) { CPUJob::cpu_time_consumed = cpu_time_consumed; }
-
-int CPUJob::getMemoryConsumed() const { return memory_consumed; }
-void CPUJob::setMemoryConsumed(int memory_consumed) { CPUJob::memory_consumed = memory_consumed; }
 
 template <class DT>
 class Queue {
-    public:
-        DT* JobPointer; // Pointer to a job (e.g., CPUJob)
-        Queue<DT>* next; // Pointer to the next node in the queue
-        Queue(); // Constructor
-        Queue(DT* JobPointer); // Constructor
-        Queue<DT>* getNext();
+public:
+    DT JobPointer; // Pointer to a job (e.g., CPUJob*)
+    Queue<DT>* next; // Pointer to the next node in the queue
+    Queue(); // Constructor
+    Queue(DT JobPointer); // Constructor
+    Queue<DT>* getNext();
 };
 
 template <class DT>
@@ -75,7 +38,7 @@ Queue<DT>::Queue() {
 }
 
 template <class DT>
-Queue<DT>::Queue(DT* JobPointer) {
+Queue<DT>::Queue(DT JobPointer) {
     this->JobPointer = JobPointer;
     next = nullptr;
 }
@@ -87,22 +50,20 @@ Queue<DT>* Queue<DT>::getNext() {
 
 template <class DT>
 class NovelQueue {
-    public:
-        ~NovelQueue(); // Destructor
-        NovelQueue(); // Default Constructor
-        Queue<DT>* front; // Pointer to the front of the queue
-        Queue<DT>** NodePtrs; // Array of pointers to Queue nodes
-        int size; // Number of elements in the queue)
-        void enqueue(DT JobPointer); // Add a new job to the queue
-        DT dequeue(); // Remove a job from the queue
-        void modify(int job_id, int new_priority, int new_job_type,
-        int new_cpu_time_consumed, int new_memory_consumed); // Modify a job
-        void change(int job_id, int field_index, int new_value); // Change a job value
-        void promote(int job_id, int positions); // Promote a job
-        NovelQueue<DT>* reorder(int attribute_index); // Reorder the queue
-        void display(); // Display the queue
-        int count(); // Count the number of elements in the queue
-        void listJobs(); // List all jobs in the queue     
+public:
+    ~NovelQueue(); // Destructor
+    NovelQueue(); // Default Constructor
+    Queue<DT>* front; // Pointer to the front of the queue
+    int size; // Number of elements in the queue
+    void enqueue(DT JobPointer); // Add a new job to the queue
+    DT dequeue(); // Remove a job from the queue
+    void modify(int job_id, int new_priority, int new_job_type, int new_cpu_time_consumed, int new_memory_consumed); // Modify a job
+    void change(int job_id, int field_index, int new_value); // Change a job value
+    void promote(int job_id, int positions); // Promote a job
+    NovelQueue<DT>* reorder(int index); // Reorder the queue
+    void display(); // Display the queue
+    int count(); // Count the number of elements in the queue
+    void listJobs(); // List all jobs in the queue
 };
 
 template <class DT>
@@ -123,7 +84,7 @@ NovelQueue<DT>::~NovelQueue() {
 
 template <class DT>
 void NovelQueue<DT>::enqueue(DT JobPointer) {
-    Queue<DT>* newNode = new Queue<DT>(&JobPointer);
+    Queue<DT>* newNode = new Queue<DT>(JobPointer);
     if (size == 0) {
         front = newNode;
     } else {
@@ -132,6 +93,7 @@ void NovelQueue<DT>::enqueue(DT JobPointer) {
             current = current->next;
         }
         current->next = newNode;
+        current->JobPointer->display();
     }
     size++;
 }
@@ -139,25 +101,23 @@ void NovelQueue<DT>::enqueue(DT JobPointer) {
 template <class DT>
 DT NovelQueue<DT>::dequeue() {
     if (size == 0) {
-        cout << "Queue is empty!" << endl;
         return nullptr;
     }
     Queue<DT>* removedNode = front;
     front = front->next;
     size--;
-    return *removedNode->JobPointer;
+    return removedNode->JobPointer;
 }
 
 template <class DT>
-void NovelQueue<DT>::modify(int job_id, int new_priority, int new_job_type,
-        int new_cpu_time_consumed, int new_memory_consumed) {
+void NovelQueue<DT>::modify(int job_id, int new_priority, int new_job_type, int new_cpu_time_consumed, int new_memory_consumed) {
     Queue<DT>* current = front;
     while (current) {
-        if (current->JobPointer->getJobId() == job_id) {
-            current->JobPointer->setPriority(new_priority);
-            current->JobPointer->setJobType(new_job_type);
-            current->JobPointer->setCpuTimeConsumed(new_cpu_time_consumed);
-            current->JobPointer->setMemoryConsumed(new_memory_consumed);
+        if (current->JobPointer->job_id == job_id) {
+            current->JobPointer->priority = new_priority;
+            current->JobPointer->job_type = new_job_type;
+            current->JobPointer->cpu_time_consumed = new_cpu_time_consumed;
+            current->JobPointer->memory_consumed = new_memory_consumed;
             return;
         }
         current = current->next;
@@ -169,22 +129,23 @@ template <class DT>
 void NovelQueue<DT>::change(int job_id, int field_index, int new_value) {
     Queue<DT>* current = front;
     while (current) {
-        if (current->JobPointer->getJobId() == job_id) {
+        if (current->JobPointer->job_id == job_id) {
             switch (field_index) {
-                case 1:
-                    current->JobPointer->setPriority(new_value);
-                    break;
-                case 2:
-                    current->JobPointer->setJobType(new_value);
-                    break;
-                case 3:
-                    current->JobPointer->setCpuTimeConsumed(new_value);
-                    break;
-                case 4:
-                    current->JobPointer->setMemoryConsumed(new_value);
-                    break;
-                default:
-                    cout << "Invalid field index!" << endl;
+            case 1:
+                current->JobPointer->priority = new_value;
+                break;
+            case 2:
+                current->JobPointer->job_type = new_value;
+                break;
+            case 3:
+                current->JobPointer->cpu_time_consumed = new_value;
+                break;
+            case 4:
+                current->JobPointer->memory_consumed = new_value;
+                break;
+            default:
+                cout << "Invalid field index!" << endl;
+                return;
             }
             return;
         }
@@ -195,45 +156,50 @@ void NovelQueue<DT>::change(int job_id, int field_index, int new_value) {
 
 template <class DT>
 void NovelQueue<DT>::promote(int job_id, int positions) {
+    if (positions <= 0 || !front) return;
+
     Queue<DT>* current = front;
-    while (current) {
-        if (current->JobPointer->getJobId() == job_id) {
-            if (current == front) {
-                cout << "Job is already at the front!" << endl;
-                return;
-            }
-            Queue<DT>* previous = front;
-            while (previous->next != current) {
-                previous = previous->next;
-            }
-            previous->next = current->next;
-            current->next = front;
-            front = current;
-            cout << "Job promoted!" << endl;
-            return;
-        }
+    Queue<DT>* previous = nullptr;
+    while (current && current->JobPointer->job_id != job_id) {
+        previous = current;
         current = current->next;
     }
-    cout << "Job not found!" << endl;
+
+    if (!current) {
+        return;
+    }
+
+    if (previous) {
+        previous->next = current->next;  // Remove current from its position
+        current->next = front;           // Move it to the front
+        front = current;
+    }
 }
 
 template <class DT>
-NovelQueue<DT>* NovelQueue<DT>::reorder(int attribute_index) {
+NovelQueue<DT>* NovelQueue<DT>::reorder(int index) {
     NovelQueue<DT>* reorderedQueue = new NovelQueue<DT>();
     Queue<DT>* current = front;
     while (current) {
-        Queue<DT>* newNode = new Queue<DT>(current->JobPointer);
-        if (reorderedQueue->size == 0) {
-            reorderedQueue->front = newNode;
+        Queue<DT>* next = current->next;
+        if (index == 1) {
+            current->next = reorderedQueue->front;
+            reorderedQueue->front = current;
         } else {
-            Queue<DT>* currentNew = reorderedQueue->front;
-            while (currentNew->next) {
-                currentNew = currentNew->next;
+            Queue<DT>* previous = nullptr;
+            Queue<DT>* temp = reorderedQueue->front;
+            while (temp && temp->JobPointer->priority > current->JobPointer->priority) {
+                previous = temp;
+                temp = temp->next;
             }
-            currentNew->next = newNode;
+            if (previous) {
+                previous->next = current;
+            } else {
+                reorderedQueue->front = current;
+            }
+            current->next = temp;
         }
-        reorderedQueue->size++;
-        current = current->next;
+        current = next;
     }
     return reorderedQueue;
 }
@@ -242,7 +208,7 @@ template <class DT>
 void NovelQueue<DT>::display() {
     Queue<DT>* current = front;
     while (current) {
-        (*current->JobPointer).display();
+        current->JobPointer->display();
         current = current->next;
     }
 }
@@ -256,10 +222,9 @@ template <class DT>
 void NovelQueue<DT>::listJobs() {
     Queue<DT>* current = front;
     while (current) {
-        cout << current->JobPointer->getJobId() << " ";
+        cout << "Job ID: " << current->JobPointer->job_id << endl;
         current = current->next;
     }
-    cout << endl;
 }
 
 int main() {
@@ -283,6 +248,7 @@ int main() {
     int positions;
     int attribute_index; // Variable for the 'Reorder' command
 
+    cout << "Start Enqueueing Jobs:" << endl;
     /************** Read each command Process ***************/
     for (int i = 0; i < n; ++i) {
         cin >> command; // Read the command type
